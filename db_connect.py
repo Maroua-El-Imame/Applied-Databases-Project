d3e1403b75ece30c6ecc550818550adc4ca940c7
Maroua El Imame <g00472980@atu.ie>
2026-05-04 23:57:37 +0100

    Update database connection setup for MySQL and Neo4j

--- db_connect.py
+++ db_connect.py
@@ -1,9 +1,18 @@
 import mysql.connector
+from neo4j import GraphDatabase
 
+# MySQL connection 
 def get_mysql_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="YOUR_PASSWORD",
+        password="sql1225**",
         database="appdbproj"
+    )
+
+# Neo4j connection
+def get_neo4j_driver():
+    return GraphDatabase.driver(
+        "bolt://localhost:7687",
+        auth=("neo4j", "neo4jneo4j")
     )
